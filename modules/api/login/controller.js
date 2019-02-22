cc59d876a4dd63af7f3d3555bff8f2be708621f8
Josue Guerrero <josue.guerrero@interaction.cr>
2019-02-21 20:28:28 -0600

    token fix

--- modules/api/login/controller.js
+++ modules/api/login/controller.js
@@ -11,11 +11,12 @@ export const register = async (req, res) => {
       passowrd: user.password
     }
     await firebase.auth().createUser(usersRef).then(response => {
-        console.log(response);
-        return res.status(200).json({ error: false, message: "Se registro:" + user.email });
+      console.log(response);
+      let token = createToken();
+      return res.status(200).json({ error: false, message: "Se registró con exito", token: token });
     }).catch(err => {
       console.log(err.errorInfo.message);
-      return res.status(400).json({ error: true, message: err.errorInfo.message});
+      return res.status(400).json({ error: true, message: err.errorInfo.message });
     })
 
   } catch (error) {
@@ -31,15 +32,7 @@ export const log_in = async (req, res) => {
     await firebase.auth().getUserByEmail(user.email).then(response => {
       console.log("login");
       console.log(response);
-      const payload = {
-        check:  true
-      };
-      var token = jwt.sign(payload, process.env.SECRET_PASSWORD, {
-        expiresIn: 10 // expires in 24 hours
-      });
-
-      console.log("token");
-      console.log(token);
+      let token = createToken();
       return res.status(200).json({ error: false, message: "Login successfull", token: token });
 
     }).catch(error => {
@@ -49,4 +42,14 @@ export const log_in = async (req, res) => {
   } catch (error) {
     return res.status(500).json({ error: true, message: error });
   }
+}
+
+const createToken = async () => {
+  const payload = {
+    check: true
+  };
+  var token = await jwt.sign(payload, process.env.SECRET_PASSWORD, {
+    expiresIn: 20 // expires in 24 hours
+  });
+  return token
 }
